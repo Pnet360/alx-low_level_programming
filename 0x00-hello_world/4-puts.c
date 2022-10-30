796222b8780e01f4a579199b558c3d9b136b59c8
Pnet360 <infotechwebng@gmail.com>
2022-10-29 17:35:39 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ *
+ * *main - Entry point
+ *
+ * *Return: Always 0 (success)
+ *
+ * */
+
+
+
+int main(void)
+
+{
+
+	Programming is like building a multilingual puzzle
+
+
+
+}
